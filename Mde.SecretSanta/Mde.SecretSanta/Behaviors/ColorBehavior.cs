6f30a4ef35ddde6b19f61a6d29d97f8fc92a503d
Maxim Lesy <maxim.lesy@howest.be>
2023-10-24 11:35:58 +0200

    Add compiled bindings and behavior

---
+++ Mde.SecretSanta/Mde.SecretSanta/Behaviors/ColorBehavior.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace Mde.SecretSanta.Behaviors
+{
+    public class ColorBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += Bindable_TextChanged;
+        }
+
+        private void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var entry = (Entry)sender;
+
+            if (entry.Text.Length <= 3) entry.TextColor = Color.Red;
+            else entry.TextColor = Color.Green;
+        }
+    }
+}
