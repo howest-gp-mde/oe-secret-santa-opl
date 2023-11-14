86a3d69a3c3f5e7da5b6135d1b2433c7aee34afc
Maxim Lesy <maxim.lesy@howest.be>
2023-11-14 11:08:21 +0100

    Add UWP play sound

--- Mde.SecretSanta/Mde.SecretSanta.UWP/Services/UwpInformerService.cs
+++ Mde.SecretSanta/Mde.SecretSanta.UWP/Services/UwpInformerService.cs
@@ -5,15 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 [assembly:Xamarin.Forms.Dependency(typeof(Mde.SecretSanta.UWP.Services.UwpInformerService))]
 namespace Mde.SecretSanta.UWP.Services
 {
     public class UwpInformerService : IInFormerService
     {
-        public void InformUser()
+        public async void InformUser()
         {
-            throw new NotImplementedException();
+            var element = new MediaElement();
+            var folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Sounds");
+            var file = await folder.GetFileAsync("bell.wav");
+            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            element.SetSource(stream, "");
+            element.Play();
         }
     }
 }
